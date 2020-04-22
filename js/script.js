8565ab478ea24b6b110c59ef7fca906ac84176b8
Long Nguyen <n.h.long.9697@gmail.com>
2020-04-22 14:39:03 +0700

    add constant

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const GAMEBOARD_WIDTH = 512;
 const GAMEBOARD_HEIGHT = 512;
 const PIPE_GAP = 85;
 const GRAVITY = 1.5;
+const JUMP = 25;
 const video = document.getElementById("video");
 const videoCanvas = document.getElementById("videoCanvas");
 const videoContext = videoCanvas.getContext("2d");
@@ -12,6 +13,7 @@ const modelParams = {
   iouThreshold: 0.5, // ioU threshold for non-max suppression
   scoreThreshold: 0.5, // confidence threshold for predictions.
 };
+const PIPE_DISTANCE = 125;
 
 const BACKGROUND_IMAGE_SRC = "./images/bg.png";
 const BIRD_IMAGE_SRC = "./images/bird.png";
@@ -29,10 +31,9 @@ function Bird() {
   this.birdImage.src = BIRD_IMAGE_SRC;
   this.jump = (event) => {
     if (event.which == 38) {
-      this.yBird -= 25;
+      this.yBird -= JUMP;
     }
   };
-
 }
 
 function Pipe(cvs) {
@@ -91,13 +92,13 @@ function Board(cvs, bird, pipe, player) {
       this.pipes[i].xPipe--;
 
       //add new pipe
-      if (this.pipes[i].xPipe == 125) {
+      if (this.pipes[i].xPipe == PIPE_DISTANCE) {
         let newPipe = new Pipe();
         this.pipes.push(newPipe);
       }
 
       //add score
-      if (this.pipes[i].xPipe == 5) {
+      if (this.pipes[i].xPipe == DEFAULT_BIRD_X) {
         this.player.score++;
       }
 
@@ -106,15 +107,15 @@ function Board(cvs, bird, pipe, player) {
         //crash the pipe
         (this.bird.xBird + this.bird.birdImage.width >= this.pipes[i].xPipe &&
           this.bird.xBird <=
-          this.pipes[i].xPipe + this.pipes[i].northPipeImage.width &&
+            this.pipes[i].xPipe + this.pipes[i].northPipeImage.width &&
           (this.bird.yBird <=
             this.pipes[i].yPipe + this.pipes[i].northPipeImage.height ||
             this.bird.yBird + this.bird.birdImage.height >=
-            this.pipes[i].yPipe +
-            this.pipes[i].northPipeImage.height.PIPE_GAP)) ||
+              this.pipes[i].yPipe +
+                this.pipes[i].northPipeImage.height.PIPE_GAP)) ||
         //crash the ground
         this.bird.yBird + this.bird.birdImage.height >=
-        GAMEBOARD_HEIGHT - foregroundImage.height
+          GAMEBOARD_HEIGHT - foregroundImage.height
       ) {
         window.location.reload(true); // reload the page
       }
@@ -134,7 +135,6 @@ function Board(cvs, bird, pipe, player) {
   };
 }
 function init() {
-
   let cvs = document.getElementById("canvas");
   let bird = new Bird();
   let pipe = new Pipe(cvs);
@@ -144,35 +144,30 @@ function init() {
   let model = null;
   document.addEventListener("keydown", bird.jump);
   // Load the model.
-  handTrack.load(modelParams).then(lmodel => {
+  handTrack.load(modelParams).then((lmodel) => {
     // detect objects in the image.
     model = lmodel;
     handTrack.startVideo(video).then((status) => {
       console.log("video started", status);
       if (status) {
-        runDetection()
+        runDetection();
       }
-      
     });
   });
-  board.start()
+  board.start();
 
   function runDetection() {
-    model.detect(video).then(predictions => {
+    model.detect(video).then((predictions) => {
       console.log("Predictions: ", predictions);
       model.renderPredictions(predictions, videoCanvas, videoContext, video);
       if (predictions.length > 0) {
-        console.log("hand found")
-        bird.yBird -= 25
-        console.log(bird.yBird)
+        console.log("hand found");
+        bird.yBird -= JUMP;
+        console.log(bird.yBird);
       }
-      isLoaded = true
+      isLoaded = true;
       requestAnimationFrame(runDetection);
     });
   }
 }
-// //fix bug later regarding score issue
-
-
-
-
+//fix bug later regarding score issue
